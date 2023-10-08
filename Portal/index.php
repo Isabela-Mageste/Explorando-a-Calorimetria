449f98800c75a62d6e900716997914d5aff3120f
Mageste <115092439+Itsukki-25@users.noreply.github.com>
2023-10-08 10:59:35 -0300

    atualização :P

---
+++ Portal/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <!-- O cabecalho do portal iria aqui -->
+	<?php include 'cabecalho.html'; ?>
+    <!-- O conteúdo do portal iria aqui -->
+	<?php include 'main1.html'; ?>
+	<!-- O rodapé do portal iria aqui -->
+	<?php include 'rodape.html'; ?>
+</body>
+</html>
