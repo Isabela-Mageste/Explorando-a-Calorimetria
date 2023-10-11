3f4b2555bb47959588859f586c1a1631274d5fef
Mageste <115092439+Itsukki-25@users.noreply.github.com>
2023-10-11 15:19:33 -0300

    inicio de um sonho

---
+++ js/content.js
@@ -0,0 +1,27 @@
+export const quizData = [
+    {
+      question:
+        "Sora é um estudante do ensino médio normal. Ele começa mais um dia escolar. Se levanta e vai tomar um banho gelado para se despertar. Qual hormônio liberado?",
+      answer: {
+        a: "Prolactina",
+        b: "Cortisol",
+        c: "Testoterona",
+        d: "Adrenalina",
+      },
+      correct: "b",
+      scene: "/Assets/Scenes/soraBath.gif",
+      video: "https://www.youtube.com/embed/Wy3QMieyN_Q",
+    },
+    {
+      question:
+        "Logo após o banho sora vai para a cozinha e se depara com sua comida preferida. Qual o hormônio liberado?",
+      answer: {
+        a: "Tiroxina",
+        b: "Adrenalina",
+        c: "Serotonina",
+        d: "Glucagon",
+      },
+      correct: "c",
+      scene: "/Assets/Scenes/soraEating.gif",
+    }
+];
