c6ff3acb5418da3aab29f9c3b76fad0881a5a675
Mageste <115092439+Itsukki-25@users.noreply.github.com>
2023-10-12 20:20:29 -0300

    modelo de arquivo de dados pronto !!!

--- js/content.js
+++ js/content.js
@@ -1,26 +1,23 @@
 export const quizData = [
     {
-      question:
-        "Sora é um estudante do ensino médio normal. Ele começa mais um dia escolar. Se levanta e vai tomar um banho gelado para se despertar. Qual hormônio liberado?",
-     item: {
-        a: "Prolactina",
-        b: "Cortisol",
-        c: "Testoterona",
-        d: "Adrenalina",
+      titulo:"Calor específico dos metais" ,
+      figura: "/assets/metais.jpg",
+      resumo: "  ",
+             item: {
+        a: "Panela ou recipiente de metal",
+        b: "Termômetro",
+        c: "Água",
+        d: "Fonte de calor",
+        e: "Panela ou recipiente de metal",
+        f: "Diferentes amostras de metais",
+        g: "Balança",
+        h: " ",
+        i: " ",
+        j: " ",
+        
       },
-      scene: "/Assets/Scenes/soraBath.gif",
+      procedimento: "1 - Coloque uma quantidade conhecida de água na panela ou recipiente de metal e aqueça-a até uma temperatura elevada,porém, abaixo do ponto de ebulição.<br> 2 - Meça a temperatura inicial da água e anote-a.<br> 3 - Com cuidado, adicione uma amostra de metal de massa conhecida na água quente.<br> 4 - Monitore e registre a temperatura final da mistura até que ela atinja o equilíbrio térmico.<br> 5 - Utilize a variação de temperatura, a massa do metal e a massa da água para calcular o calor específico do metal. " ,
       video: "https://www.youtube.com/embed/Wy3QMieyN_Q",
     },
-    {
-      question:
-        "Logo após o banho sora vai para a cozinha e se depara com sua comida preferida. Qual o hormônio liberado?",
-      answer: {
-        a: "Tiroxina",
-        b: "Adrenalina",
-        c: "Serotonina",
-        d: "Glucagon",
-      },
-      correct: "c",
-      scene: "/Assets/Scenes/soraEating.gif",
-    }
+    
 ];
