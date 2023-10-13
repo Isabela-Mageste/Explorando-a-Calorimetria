06248ad75137bb6cb84aa075a516650f5302fa2f
Mageste <115092439+Itsukki-25@users.noreply.github.com>
2023-10-13 09:24:47 -0300

    teste

--- js/content.js
+++ js/content.js
@@ -15,7 +15,7 @@ export const quizData = [
         i: " ",
         j: " ",
         
-      },
+      }, 
       procedimento: "1 - Coloque uma quantidade conhecida de água na panela ou recipiente de metal e aqueça-a até uma temperatura elevada,porém, abaixo do ponto de ebulição.<br> 2 - Meça a temperatura inicial da água e anote-a.<br> 3 - Com cuidado, adicione uma amostra de metal de massa conhecida na água quente.<br> 4 - Monitore e registre a temperatura final da mistura até que ela atinja o equilíbrio térmico.<br> 5 - Utilize a variação de temperatura, a massa do metal e a massa da água para calcular o calor específico do metal. " ,
       video: "https://www.youtube.com/embed/Wy3QMieyN_Q",
     },
