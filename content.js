da60642efee3e74f8a3e584279fbdadbb848924f
Sarah Rabelo Araujo <117447148+LoiraDoTchan@users.noreply.github.com>
2024-02-22 16:39:17 -0300

    update legal

---
+++ content.js
@@ -0,0 +1,126 @@
+export const data = [
+  {
+    titulo: "Termometro Caseiro",
+    figura: "/assets/termometro.jfif",
+    resumo: " ",
+    item: {
+      a: "Garrafa de plástico pequena",
+      b: "Água colorida",
+      c: "Canudo de plástico",
+      d: "Supercola ou fita adesiva",
+      e: "Marcador ou etiquetas para a escala",
+      f: null,
+      g: null,
+      h: null,
+      i: null,
+      j: null,
+    },
+    procedimento:
+      "1- Lave e seque bem a garrafa de plástico.<br>2 - Encha cerca de um quarto da garrafa com água colorida. Isso servirá como o líquido dentro do termômetro.<br>3 - Coloque o canudo dentro da garrafa, deixando-o submerso na água, mas não encostando no fundo.<br> 4 - Cole o canudo na posição com supercola ou use fita adesiva para mantê-lo no lugar.<br> 5 - Marque a posição inicial da água no canudo com um marcador ou etiqueta. Isso será a referência para a temperatura ambiente.<br> 6 - Coloque o termômetro em diferentes condições de temperatura para observar como a água sobe ou desce no canudo. Marque as diferentes posições da água com o marcador ou etiquetas.<br> 7 - Agora você tem um termômetro caseiro simples!",
+    video: "https://www.youtube.com/embed/Wy3QMieyN_Q",
+  },
+  {
+    titulo: "Calor específico dos metais",
+    figura: "/assets/metais.jpg",
+    resumo: " ",
+    item: {
+      a: "Panela ou recipiente de metal",
+      b: "Termômetro",
+      c: "Água",
+      d: "Fonte de calor",
+      e: "Panela ou recipiente de metal",
+      f: "Diferentes amostras de metais",
+      g: "Balança",
+      h: null,
+      i: null,
+      j: null,
+    },
+    procedimento:
+      "1 - Coloque uma quantidade conhecida de água na panela ou recipiente de metal e aqueça-a até uma temperatura elevada,porém, abaixo do ponto de ebulição.<br> 2 - Meça a temperatura inicial da água e anote-a.<br> 3 - Com cuidado, adicione uma amostra de metal de massa conhecida na água quente.<br> 4 - Monitore e registre a temperatura final da mistura até que ela atinja o equilíbrio térmico.<br> 5 - Utilize a variação de temperatura, a massa do metal e a massa da água para calcular o calor específico do metal. ",
+    video: "https://www.youtube.com/embed/Wy3QMieyN_Q",
+  },
+  {
+    titulo: "Fusão de gelo",
+    figura: "/assets/gelo.jpg",
+    resumo:
+      "Determinar o calor latente de fusão do gelo usando um calorímetro caseiro.",
+    item: {
+      a: "Recipiente isolante (pode ser uma caixa de isopor ou um pote térmico)",
+      b: "Água",
+      c: "Gelo",
+      d: "Fonte de calor",
+      e: "Termometro",
+      f: null,
+      g: null,
+      h: null,
+      i: null,
+      j: null,
+    },
+    procedimento:
+      "1 - Coloque uma quantidade conhecida de gelo em <br> um recipiente isolante.<br> 2 - Meça a temperatura inicial do gelo e anote-a.<br> 3 - Aqueça uma pequena quantidade de água até uma<br> temperatura próxima ao ponto de ebulição.<br> 4 - Despeje a água quente sobre o gelo no recipiente<br> isolante.<br> 5 - Monitore e registre a temperatura da mistura até<br> que todo o gelo tenha derretido.<br> 6 - Utilize a variação de temperatura e a quantidade de <br> gelo derretido para calcular o calor latente de fusão do gelo.<br>",
+    video: "https://www.youtube.com/embed/kfR3QFhdfoo",
+  },
+
+  {
+    titulo: "Estado da matéria",
+    figura: " ",
+    resumo:
+      "A matéria é tudo o que ocupa espaço e possui massa. Ela pode ser encontrada em diversos estados, dependendo das condições de temperatura e pressão a que está sujeita. Os três estados mais comuns da matéria são sólido, líquido e gasoso, cada um deles com características únicas que definem suas propriedades e comportamentos. Esses estados da matéria são fundamentais para a compreensão da física e da química, pois afetam o comportamento e as propriedades das substâncias em nosso mundo. <br> Sólido:<br> Quando algo é sólido, é como uma pedra ou gelo, por exemplo. Ele mantém a mesma forma o tempo todo e você não pode espremê-lo ou moldá-lo facilmente. Os átomos(menores partículas que formam a matéria) ou pedaços que compõem o sólido ficam bem juntinhos e não se movem muito. <br> Líquido:<br> Os líquidos são como a água, o suco ou o leite. Eles não têm uma forma fixa e podem se espalhar para preencher qualquer espaço. Imagine despejar água em um copo : ela assume a forma do copo. Os átomos ou partículas em um líquido ficam um pouco mais soltos do que em um sólido, mas ainda assim mais unidos que em um gás. <br> Gasoso: <br> O estado gasoso é como o ar que respiramos. Os gases não têm forma nem volume fixos, eles se espalham para ocupar todo o espaço disponível. As partículas em um gás estão muito afastadas e se movem rapidamente e aleatoriamente(como se estivessem dançando)",
+    item: {
+      a: "Panela",
+      b: "Água",
+      c: "Gelo",
+      d: "Fonte de calor",
+      e: "Termometro",
+      f: "Sal",
+      g: "Caneta e papel para registro de observações",
+      h: "Copos",
+      i: null,
+      j: null,
+    },
+    procedimento:
+      "Parte 1: Fusão (Mudança de Sólido para Líquido) <br>Coloque meio copo de água em uma panela e coloque-a no fogão. <br>Aqueça a água até que comece a ferver (100°C).<br>Registre a temperatura da água enquanto ela aquece. <br>Observe o que acontece com a água à medida que ela aquece e registre suas observações. Você deve notar a formação de bolhas e vapor. <br><br> Parte 2: Solidificação (Mudança de Líquido para Sólido)<br>Coloque um pouco de água em recipiente de plástico e coloque-o no freezer ou congelador. <br>Deixe a água congelar por algumas horas.<br>Retire o recipiente e observe o que aconteceu com a água. Toque no gelo e registre suas observações.<br><br>Parte 3: Vaporização (Mudança de Líquido para Gasoso)<br>Despeje uma pequena quantidade de água em um copo transparente<br>Aqueça o copo com água no micro-ondas por cerca de 30 segundos.Observe o que acontece com a água e registre suas observações. Você deve ver a formação de vapor na parte interna do copo.<br><br> Parte 4: Condensação (Mudança de Gasoso para Líquido)<br>Encha outro copo com água quente. Coloque o copo em um ambiente frio, como uma janela ou geladeira <br>Observe o que acontece com o copo e registre suas observações. Deve haver gotas de água formando-se na parte externa do copo.<br><br>Parte 5: Experimento Adicional - Efeito do Sal na Solidificação<br>Repita o procedimento da Parte 2, mas desta vez adicione uma pequena quantidade de sal à água antes de congelá-la.<br>Compare o tempo que leva para a água congelar com e sem sal. Registre suas observações.<br>Análise de Resultados:<br>1- Compare as mudanças de estado físico da água nas diferentes partes do experimento.<br>2-Discuta as observações e explique os conceitos por trás das mudanças de estado, como temperatura, pressão e energia. <br>3-Comente sobre como o sal afetou o processo de solidificação",
+    video: " ",
+  },
+  {
+    titulo: "Transferência de calor",
+    figura: " ",
+    resumo:
+      "A transferência de calor é exatamente essa transferência de energia que pode fazer com que um objeto mude de estado físico, e ela pode acontecer por três modos diferentes:<br>Condução:<br>Imagine você segurando uma barra de metal quente em uma ponta. A energia térmica no metal começa a se espalhar ao longo da barra em uma espécie de 'corrida quente'. É como uma partida de revezamento de partículas, onde as mais agitadas passam o bastão (energia) para as próximas. A energia térmica segue essa corrida até que a velocidade das partículas diminua. Quando dois objetos atingem a mesma temperatura, eles atingem o equilíbrio térmico. Confira aqui um experimento que mostra a condução!<br>Convecção:<br>Agora, vamos pensar em uma panela de água fervente. A água quente perto do fundo da panela começa a fazer um movimento para cima. À medida que a água quente sobe, a água fria desce para tomar o seu lugar. Isso cria um movimento constante, espalhando o calor pela panela. Isso também acontece com as correntes de ar, ou seja, o ar quente sobe e o ar frio desce! <br>Irradiação:<br>A irradiação é como um show de luzes no espaço. Imagine estar ao sol, sentindo o calor acariciando sua pele. Isso acontece porque o sol está disparando ondas de energia térmica, como raios de calor e luz, diretamente para a Terra. Não há necessidade de um mensageiro ou de um meio material; o calor vem diretamente até você, como um abraço quente do sol. Assim sendo, percebe-se que não há contato! Ela pode também ser observada em uma fogueira: a maioria das pessoas não tem que entrar dentro dela para se aquecer! A irradiação é o único modo de transferir calor no vácuo do espaço.<br>Essas são as maneiras pelas quais o calor é transmitido. Ter um bom entendimento desses processos é fundamental, desde cozinhar uma refeição saborosa até entender o funcionamento de máquinas e sistemas de aquecimento em nossas casas. É como um verdadeiro espetáculo de ciência em ação!",
+    item: {
+      a: "Três copos transparentes",
+      b: "Corante alimentar(opcional)",
+      c: "Água",
+      d: "Gelo(opcional)",
+      f: "Fonte de calor",
+      g: "Caneta e papel para registro de observações",
+      h: null,
+      i: null,
+      j: null,
+    },
+    procedimento:
+      "Parte 1: Condução<br>Encha um recipiente com água quente (não ferva) e adicione algumas gotas de corante alimentar (opcional).<br>Encha outro recipiente com água fria. Coloque um pedaço de metal ou plástico no recipiente com água quente.<br>Coloque um pedaço de metal ou plástico no recipiente com água fria. Observe como o calor é transferido através dos materiais. Registre suas observações.<br><br>Parte 2: Convecção<br>a convecção pode ser observada no funcionamento de uma panela de pressão. agora, adaptando para uma atividade mais segura, faremos o seguinte:<br>Aproveitando a última atividade, pegue um terceiro copo e encha metade com a água quente e metade com a água fria. Observe que a água quente sobe e a água fria desce.<br><br>Parte 3: Radiação Térmica<br>Encha um recipiente com água em temperatura ambiente. Coloque o recipiente em uma área com sol.Observe como o calor do sol ou da fonte de calor aquece a água no recipiente, mesmo sem contato direto. Registre suas observações.<br><br>Parte 4: Experimento Adicional - Transferência de Calor com Gelo<br>Coloque um cubo de gelo em cada um dos recipientes usados nas partes anteriores do experimento, e outro em um local com luz do Sol.<br>Observe como o gelo se comporta em cada um após dois minutos.Registre suas observações. <br>Qual tipo de transferência de calor aconteceu em cada caso?<br>Qual a temperatura da água em cada copo após o derretimento do gelo? Use o termômetro caseiro.<br>Análise de Resultados:<br>1- Reflita sobre os três métodos de transferência de calor (condução, convecção e radiação) observados no experimento. <br>2-Qual a importância da transferência de calor em processos naturais e cotidianos?",
+    video: " ",
+  },
+  {
+    titulo: "Materiais isolantes e materiais condutores",
+    figura: " ",
+    resumo:
+      "A capacidade dos materiais de transmitir ou armazenar calor depende do tipo de material. Alguns materiais podem segurar muito calor sem esquentar muito rápido, como a água, enquanto outros aquecem ou esfriam rapidamente, como os metais. Essas diferenças na capacidade de transmitir ou armazenar calor são fundamentais para diversos processos e aplicações na vida cotidiana e na indústria. .Os condutores são aqueles que transmitem o calor com maior facilidade, já os isolantes, dificultam sua saída. <br>Materiais Condutores: <br>Os materiais condutores são como 'corredores de calor'. Eles são bons em transferir calor rapidamente. Pense em uma colher de metal tocando seu café. O calor do café é absorvido pela colher e viaja rapidamente para sua mão, o que pode fazer você sentir calor. <br>Exemplos de materiais condutores incluem metais como ferro, alumínio e cobre. Isso é por isso que panelas são feitas de metal; elas espalham o calor uniformemente para cozinhar alimentos de maneira eficaz.<br>Materiais Isolantes:<br>Por outro lado, os materiais isolantes são como 'guardiões do frio'. Eles não deixam o calor passar facilmente. Pense em uma caixa de isopor. Quando cheia de gelo, por exemplo, pode deixar o que for colocado dentro dela a baixa temperatura por um longo período de tempo, pois impede o calor de fora da caixa de alcançar o que está dentro. Isso também funciona com um cobertor, que mantém o calor dentro dele e impede que este se disperse no ambiente.<br>Exemplos de materiais isolantes incluem madeira, plástico, espuma e lã. Esses materiais são usados em isolamento de casas, casacos de inverno e até em luvas de forno, para proteger nossas mãos do calor.",
+    item: {
+      a: "1 colher de metal",
+      b: "1 colher de madeira",
+      c: "1 copo com água muito gelada",
+      d: "1 copo com água quente",
+      e: "Luvas de proteção ou um pano de prato",
+      f: "Termômetro",
+      g: "Cronômetro",
+      h: "Caneta e papel para registro de observações",
+      i: null,
+      j: null,
+    },
+    procedimento:
+      "Parte 1: Preparação<br>Prepare todos os objetos(colheres) em temperatura ambiente. Certifique-se de tomar cuidado para manusear os materiais durante o experimento. Certifique-se da presença de um adulto responsável. <br><br>Parte 2: Teste de Condução de Calor<br>Meça a temperatura inicial da colher de metal e registre no papel. Mergulhe a colher no copo de água quente por cerca de 1 minuto - proteja sua mão com o pano Remova a colher do calor, meça a temperatura dela e registre no papel.Coloque a colher em um recipiente com água fria.Registre o tempo que a colher demora para esfriar até os 20 °C. <br><br>Parte 3: Teste de Isolantes Térmicos<br>Meça a temperatura inicial da colher de madeira e registre no papel. Mergulhe a colher no copo de água quente até que ela alcance a mesma temperatura que a colher de metal quando aquecida - proteja sua mão com o pano! Remova a colher do calor e registre no papel o tempo que ela levou para chegar até aquela temperatura.Coloque a colher em um recipiente com água fria.Registre o tempo que a colher demora para esfriar até os 20 °C.<br>Análise de Resultados:<br>1-Compare os tempos de aquecimento e  resfriamento da colher  metálica com o da colher de madeira.Você percebeu como os materiais metálicos, sendo condutores de calor, resfriam mais rapidamente do que os materiais isolantes?<br>2- Explique a diferença entre materiais condutores e isolantes, com base  no experimento e usando suas próprias palavras",
+    video: " ",
+  },
+];
--- js/app.js
+++ js/app.js
@@ -1,82 +1,59 @@
- import { quizData } from "/js/content.js";
+import { data } from "/js/content.js";
 
 // Declaracao de variaveis para localizacao de partes do HTML
-const questionScene = document.querySelector(".quiz-scene");
-const quiz = document.querySelector(".quiz-container");
-const questionElement = document.querySelector(".quiz-header");
-const answerElements = document.querySelectorAll(".answer");
-const questionNumber = document.querySelector("title");
+const page = document.querySelector(".page");
+const titulo = document.querySelector(".titulo");
+const figura = document.querySelector(".figura");
+const resumo = document.querySelector(".resumo");
+const item = document.querySelectorAll(".item");
+const procedimentoText = document.querySelector(".procedimento");
+const video = document.querySelector(".video-wrapper");
 
-// Declaracao de variaveis para localizacao das respostas
-const a_text = document.getElementById("text_a");
-const b_text = document.getElementById("text_b");
-const c_text = document.getElementById("text_c");
-const d_text = document.getElementById("text_d");
-const e_text = document.getElementById("text_e");
-const f_text = document.getElementById("text_f");
-const g_text = document.getElementById("text_g");
-const h_text = document.getElementById("text_h");
-const i_text = document.getElementById("text_i");
-const j_text = document.getElementById("text_j");
+// Funcao que retorna o valor de um parametro na URL para carregar os dados da pagina
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+    
+  if (!results) return null;
+  if (!results[2]) return "";
 
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
-// Botao para mudar a pergunta
-const submitButton = document.getElementById("submit");
+// Funcao que carrega os dados da paginas
+function loadPage(selectedPage) {
+  // Pega os dados da pagina atual de dentro do objeto data
+  const currentData = data[selectedPage];
 
-// Variaveis para o timer
-const display = document.querySelector(".timer");
-let timer;
+  // Atualiza o titulo, a imagem, o resumo da pagina
+  titulo.innerHTML = currentData.titulo;
+  figura.innerHTML = `<img src="${currentData.figura}" style="max-width: 25vw" />`; // Se não colocar o style com max-width a formatação buga
+  resumo.innerHTML = currentData.resumo;
 
-// Variaveis que contam qual a questao atual e quantos pontos marcados
-let currentQuiz = 0;
-let score = 0;
+  // Loop para carregar os dados da checklist
+  const items = Object.keys(currentData.item);
+  items.forEach((key) => {
+    const element = document.getElementById(key);
+    const textElement = document.getElementById(`text_${key}`);
 
-// Funcao para deselecionar as repostas
-const deselectAnswers = () => {
-  // Inicio da funcao
-  answerElements.forEach((answer) => (answer.checked = false));
-};
-// Fim da funcao
-
-// Funcao para armazenar qual resposta foi marcada
-const getSelected = () => {
-  // Inicio da funcao
-  let answer;
-
-  answerElements.forEach((answerElements) => {
-    if (answerElements.checked) {
-      answer = answerElements.id;
+    if (element && textElement) {
+      const value = currentData.item[key];
+      if (value === "" || value === null) {
+        element.style.display = "none";
+        textElement.style.display = "none";
+      } else {
+        textElement.innerText = value;
+      }
     }
   });
 
-  return answer;
-};
-// Fim da funcao
+  // Atualiza o texto de procedimento
+  procedimentoText.innerHTML = currentData.procedimento;
 
-// Funcao que carrega a pergunta e as respostas do quiz
-const loadQuiz = () => {
-  // Inicio da funcao
-  deselectAnswers();
+  // Atualiza o iframe com o video correto
+  video.innerHTML = `<iframe src="${currentData.video}"/>`;
+}
 
-  const currentQuizData = quizData[currentQuiz];
-
-  questionNumber.innerHTML = `STN - Fase ${currentQuiz + 1}`; // Muda o titulo da pagina de acordo com o numero da questao
-  questionScene.innerHTML = `<img src="${currentQuizData.scene}"/>`; // Troca a imagem da cena de acordo com a cena
-  questionElement.innerHTML = currentQuizData.question; // Carrega a pergunta
-
-  // Carrega as repostas para a pergunta
-  a_text.innerText = currentQuizData.answer.a;
-  b_text.innerText = currentQuizData.answer.b;
-  c_text.innerText = currentQuizData.answer.c;
-  d_text.innerText = currentQuizData.answer.d;
-  e_text.innerText = currentQuizData.answer.e;
-  f_text.innerText = currentQuizData.answer.f;
-  g_text.innerText = currentQuizData.answer.g;
-  h_text.innerText = currentQuizData.answer.h;
-  i_text.innerText = currentQuizData.answer.i;
-  j_text.innerText = currentQuizData.answer.j;
- 
-
-};
-// Fim da funcao
-
+loadPage(getParameterByName("experimento"));
